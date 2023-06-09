3623ef5dc3c396db5e793c0d75611af2e79943ed
Konstantin <turchakkonstantin@gmail.com>
2023-06-09 15:39:51 +0300

    finished

--- Multitrading _from_array/Program.cs
+++ Multitrading _from_array/Program.cs
@@ -9,13 +9,14 @@ namespace Multitrading__from_array
 {
     internal class Program
     {
+        static Semaphore semaphore = new Semaphore(10, 10);
         static void Main(string[] args)
         {
             int size;
-            
+           
             while (true)
             {
-                Console.Write("Введите количество элементов массива: ");
+                Console.Write("Введите количество элементов массива (введите более 10 чтобы заметить разницу): ");
 
                 if (int.TryParse(Console.ReadLine(), out size)) break;
                 else Console.WriteLine("Ошибка преобразования чтроки в число");
@@ -27,9 +28,13 @@ namespace Multitrading__from_array
             for (int i = 0; i < arr.Length; i++)
                 arr[i] = rand.Next(0, 50);
 
+
             PrintArray(arr);
 
-            IncrementArray(arr, 100);
+            IncrementArray(arr);
+
+            //Ожидание завершения всех потоков
+            Thread.Sleep(size * 1000 / 7);
 
             PrintArray(arr);
 
@@ -37,32 +42,44 @@ namespace Multitrading__from_array
 
 
 
-            void IncrementArray(int[] _arr, int threadCount)
+
+
+
+            void IncrementArray(int[] _arr)
             {
                 for (int i = 0; i < _arr.Length; i++)
                 {
                     Thread thread = new Thread(Increment);
-                    thread.Name = i.ToString();
-                    thread.Start(i);                    
+                    thread.Name = (i + 1).ToString();
+                    thread.Start(i);
                 }
             }
 
             void Increment(object index)
             {
+                semaphore.WaitOne();
+
+                Console.WriteLine($"Поток {Thread.CurrentThread.Name} запущен");
+
                 Thread.Sleep(1000);
 
-                if(index is int)
-                    arr[(int)index]++;
+                arr[(int)index]++;
 
-                Console.WriteLine($"Поток {Thread.CurrentThread.Name} выполнен");
+                Console.WriteLine($"Поток {Thread.CurrentThread.Name} выполнен!");
+
+                semaphore.Release();
             }
 
             void PrintArray(int[] array)
             {
                 Console.WriteLine("\n\n");
 
-                foreach (var item in array)
-                    Console.Write($"{item}\t");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (i % 10 == 0) Console.WriteLine("\n");
+
+                    Console.Write($"{array[i]}\t");
+                }                    
 
                 Console.WriteLine("\n\n");
             }
