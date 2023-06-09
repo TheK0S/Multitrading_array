b44ee6d24405dd485f002dc5219852097ed0667d
Konstantin <turchakkonstantin@gmail.com>
2023-06-09 12:34:04 +0300

    test version

--- Multitrading _from_array/Program.cs
+++ Multitrading _from_array/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Multitrading__from_array
@@ -26,9 +27,45 @@ namespace Multitrading__from_array
             for (int i = 0; i < arr.Length; i++)
                 arr[i] = rand.Next(0, 50);
 
+            PrintArray(arr);
 
+            IncrementArray(arr, 100);
+
+            PrintArray(arr);
 
             Console.ReadKey();
+
+
+
+            void IncrementArray(int[] _arr, int threadCount)
+            {
+                for (int i = 0; i < _arr.Length; i++)
+                {
+                    Thread thread = new Thread(Increment);
+                    thread.Name = i.ToString();
+                    thread.Start(i);                    
+                }
+            }
+
+            void Increment(object index)
+            {
+                Thread.Sleep(1000);
+
+                if(index is int)
+                    arr[(int)index]++;
+
+                Console.WriteLine($"Поток {Thread.CurrentThread.Name} выполнен");
+            }
+
+            void PrintArray(int[] array)
+            {
+                Console.WriteLine("\n\n");
+
+                foreach (var item in array)
+                    Console.Write($"{item}\t");
+
+                Console.WriteLine("\n\n");
+            }
         }
     }
 }
